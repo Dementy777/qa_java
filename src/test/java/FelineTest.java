2bfd8a2a052da215ff84960aa0bce8024293bc1a
Dementy Sivakov <dsivakov@yandex.ru>
2025-10-25 21:25:30 +0300

    Создан тестовый класс для тестирования методов в Feline и отформатирован код

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -3,6 +3,7 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -12,17 +13,17 @@ public class FelineTest {
     private Feline feline; //создал объект для тестов
 
     @Before
-    public void setUp(){  //метод создающий экземпляр Feline перед каждым тестом
-        feline=new Feline();
+    public void setUp() {  //метод создающий экземпляр Feline перед каждым тестом
+        feline = new Feline();
     }
 
 
     @Test
-    public void testEatMeat()  { //тест для метода eatMeat()
-        try{
-            List<String> foodList=feline.eatMeat();
+    public void testEatMeat() { //тест для метода eatMeat()
+        try {
+            List<String> foodList = feline.eatMeat();
             Assert.assertNotNull(foodList);
-            assertEquals(3,foodList.size());
+            assertEquals(3, foodList.size());
             Assert.assertTrue(foodList.contains("Животные"));
             Assert.assertTrue(foodList.contains("Птицы"));
             Assert.assertTrue(foodList.contains("Рыба"));
@@ -30,25 +31,26 @@ public class FelineTest {
             Assert.fail("Не ожидалось исключение при вызове eatMeat()");
         }
     }
-@Test
-    public void testGetFamily(){  //тест для проверки метода getFamily()
 
-        String family=feline.getFamily();
-        assertEquals("Кошачьи",family);
-}
+    @Test
+    public void testGetFamily() {  //тест для проверки метода getFamily()
 
-@Test
-    public void testGetKittensDefault(){ // тест для проверки метода getKittens() без параметра и с дефолтным параметром
-        int kittens=feline.getKittens();
-        Assert.assertEquals(1,kittens);
-}
-@Test
-    public void testGetKittensWihtParameters(){ //тест для проверки метода getKittens() с параметром
-        int testValue=5;
-        int kittens=feline.getKittens(testValue);
-        Assert.assertEquals(testValue,kittens);
+        String family = feline.getFamily();
+        assertEquals("Кошачьи", family);
+    }
 
-}
+    @Test
+    public void testGetKittensDefault() { // тест для проверки метода getKittens() без параметра и с дефолтным параметром
+        int kittens = feline.getKittens();
+        Assert.assertEquals(1, kittens);
+    }
+
+    @Test
+    public void testGetKittensWithParameters() { //тест для проверки метода getKittens() с параметром
+        int testValue = 5;
+        int kittens = feline.getKittens(testValue);
+        Assert.assertEquals(testValue, kittens);
+    }
 
 
 }
