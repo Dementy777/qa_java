7481b8df9df5ec4914c4662f420d9e828bc90f32
Dementy Sivakov <dsivakov@yandex.ru>
2025-10-28 15:41:56 +0300

    Создан тестовый класс для тестирования методов класса Cat

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,18 +1,13 @@
 import com.example.Cat;
-import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-
-    @Mock
-    private Feline mockFeline;
 
     @InjectMocks
     private Cat cat;
